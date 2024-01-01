d944fcc399c258e2f5e6d06dac8e2fd873a14847
Craig Eaton <craigea92@gmail.com>
2024-01-01 16:32:14 +0000

    lesson 02 complete

--- chapter_01/lesson_01.py
+++ chapter_01/lesson_01.py
@@ -1,26 +1,8 @@
 """
-FIX YOUR FIRST BUG
-The combat system in Fantasy Quest isn't working as intended! 
-It appears that players are gaining health when attacked instead of losing it.
+WELCOME TO "LEARN PYTHON"
+
+We're going to build a text-based adventure game called "Fantasy Quest" in this course! 
+I've written the first line of code for you, it prints a message welcoming new players to the game.
 """
 
-# Assignment
-# Fix the math bug in this code.
-
-sword_damage = 10
-player_health = 100
-health_after_attack = player_health + sword_damage
-
-print(f"Lollilfred's health is: {player_health}")
-print(f"Lollilfred is hit by a sword for {sword_damage} damage...")
-print(f"Lollilfred's health is now: {health_after_attack}")
-
-
-# Solution
-sword_damage = 10
-player_health = 100
-health_after_attack = player_health - sword_damage
-
-print(f"Lollilfred's health is: {player_health}")
-print(f"Lollilfred is hit by a sword for {sword_damage} damage...")
-print(f"Lollilfred's health is now: {health_after_attack}")
+print("Welcome to Fantasy Quest!")

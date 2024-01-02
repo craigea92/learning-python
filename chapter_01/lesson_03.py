f63d853d9c950cfe26c29f36d3a3bd3f74f79224
Craig Eaton <craigea92@gmail.com>
2024-01-02 09:32:14 +0000

    lesson 03 complete

---
+++ chapter_01/lesson_03.py
@@ -0,0 +1,14 @@
+"""
+THE CONSOLE
+
+The "console" is a panel that displays important messages, like errors or the text output of a program. 
+If we want to see what's happening in our code, we need to print it to the console by using the print() function.
+
+We'll learn more about functions later, but for now, just know that the print() function will print anything you put inside its parentheses.
+"""
+
+# Assignment
+# Print the following text to the console: "Use the arrow keys to move"
+
+#Solution
+print("Use the arrow keys to move")

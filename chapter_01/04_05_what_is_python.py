c8f8ba25cd8bb520a86c096712432b14a81167cc
Craig Eaton <craigea92@gmail.com>
2024-01-03 09:32:14 +0000

    lesson 04 and 05 complete

---
+++ chapter_01/04_05_what_is_python.py
@@ -0,0 +1,39 @@
+"""
+WHAT IS PYTHON?
+
+Thousands of students start their coding journey right here with Python. 
+We think it's the best programming language to get started with. 
+Python is famous for being a simple language that's easy to read and write.
+
+However, just because it's simple that doesn't mean it's not useful! 
+Python is an extremely popular language in the industry, and is well-known for:
+
+Backend web servers
+DevOps and cloud engineering
+Machine learning
+Scripting and automation
+etc...
+
+On the other hand, it's not particularly well-known for front-end work. 
+While it's possible to do so, Python isn't typically used to build visual user interfaces.
+"""
+
+# Question 01
+
+# Python is NOT often used in... 
+
+# Machine Learning
+# Front-end apps 
+# Backend servers
+# Automation scripts
+
+# Answer = Front-end apps
+
+# Question 02
+
+# Python is known for being...
+
+# Simple to read and write
+# Complex to read and write
+
+# Answer = Simple to read and write

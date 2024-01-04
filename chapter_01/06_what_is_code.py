a75b8d81012ddcfdbd7d351c88097322f357d906
Craig Eaton <craigea92@gmail.com>
2024-01-04 08:32:14 +0000

    lesson 06 complete

---
+++ chapter_01/06_what_is_code.py
@@ -0,0 +1,31 @@
+"""
+WHAT IS CODE
+
+Code is just a series of instructions that computers can folow. 
+Computers obey each instruction, one after another.
+
+Programs can be comprised of many instructions.
+Like many, Like millions.
+
+Addition is one of the most common instructions in coding.
+"""
+
+# PRINTING NUMBERS
+
+# print() can print text using quotes:
+print("some text here")
+
+# but it can also print numbers without quotes:
+print(1)
+
+# and you can do math directly inside the parenthesis:
+print(1 + 2)
+
+# Assignment
+# Calculate and print the result of 250 + 75
+
+# Solution
+print(250 + 75)
+
+# Console Output
+325

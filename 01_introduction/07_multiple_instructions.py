46411880be05f1d9bc18eb557a0a2acb51382536
Craig Eaton <craigea92@gmail.com>
2024-01-05 08:32:14 +0000

    lesson 07 complete and tidied files

---
+++ 01_introduction/07_multiple_instructions.py
@@ -0,0 +1,29 @@
+"""
+MULTIPLE INSTRUCTIONS
+
+Code runs in order, starting at the top of the program. For example:
+
+print("this prints first")
+print("this prints second")
+print("this prints last")
+
+Each print() instruction prints on a new line.
+
+The dialogue in our game needs to be printed in the correct order! 
+In the first level, our hero encounters a talking owl named Jax. 
+Let's display their conversation to the user through text.
+"""
+
+# Assignment
+# Rearrange the code so that it prints in the correct order.
+
+print("Hero: ...")
+print("Jax: Where are you off to this morning? Bkaw...")
+print("Hero: Where did an owl learn to speak??")
+print("Jax: B-Kaw!")
+
+# Solution
+print("Jax: B-Kaw!")
+print("Hero: ...")
+print("Jax: Where are you off to this morning? Bkaw...")
+print("Hero: Where did an owl learn to speak??")

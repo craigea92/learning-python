fbca3ab18fae3767f3e39749fe99a2e59d1b1036
Craig Eaton <craigea92@gmail.com>
2024-01-07 08:32:14 +0000

    lesson 09 and 10 complete

---
+++ 01_introduction/09_10_syntax_errors_quiz.py
@@ -0,0 +1,41 @@
+"""
+SYNTAX ERRORS QUIZ
+
+Syntax in programming are the rules that define what properly structured expressions and statements look like in that language. 
+For example, in Python, the following would be considered correct syntax:
+
+print("hello world")
+
+While in a different programming language, like Go, the correct syntax would be:
+
+fmt.Println("hello world")
+
+Code can have many different problems that prevent it from working as intended. 
+
+Some examples include:
+A bug in the logic. For example, a program that should add numbers together, but instead multiplies them.
+A problem with speed. A program that calculates how to play the perfect game of chess might never be able to finish because it requires too many calculations.
+A problem with syntax. This is the most common problem for new developers. 
+Luckily the Python interpreter will try to give you a descriptive error message in the console to help you find the problem.
+"""
+
+# Question 01
+
+# What does syntax mean?
+
+# The rules for valid code in a programming language
+# Fast code
+# Code that produces the correct output
+# Code in the browser
+
+# Answer = The rules for valid code in a programming language
+
+# Question 02
+
+# What happens if you try to run code with invalid syntax?
+
+# You'll be provided a syntax error message and the code won't execute
+# The network will disconnect
+# The computer will freeze
+
+# Answer = You'll be provided a syntax error message and the code won't execute

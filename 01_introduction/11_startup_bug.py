0881ba79447ff6782c1a2a359960802a09b7b499
Craig Eaton <craigea92@gmail.com>
2024-01-08 08:32:14 +0000

    lesson 11 complete

---
+++ 01_introduction/11_startup_bug.py
@@ -0,0 +1,16 @@
+"""
+STARTUP BUG
+
+A new startup has a bug in its server code. 
+The code is supposed to print messages indicating the server has started successfully.
+"""
+
+# Assignment
+# Fix the 2 errors in the code and get it to run!
+
+print("Starting up server...')
+prnt("local server is listening on port 8080")
+
+# Solution
+print("Starting up server...")
+print("local server is listening on port 8080")

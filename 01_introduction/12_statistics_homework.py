764aa4fa17de480fc866483c2328a6b46a4156b2
Craig Eaton <craigea92@gmail.com>
2024-01-09 08:32:14 +0000

    lesson 12 complete

---
+++ 01_introduction/12_statistics_homework.py
@@ -0,0 +1,14 @@
+"""
+STATISTICS HOMEWORK
+
+A local high school student was asked to do some statistics work in Python. 
+She's trying to calculate the average number of students at all of the local high schools.
+"""
+
+# Assignment
+# Debug the code so that it correctly prints the solution
+
+print("(250 + 241 + 244 + 255) / 4")
+
+# Solution
+print((250 + 241 + 244 + 255) / 4)

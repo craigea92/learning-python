65cc7804ed6dfe2e693e305d29c7b4b0afbfdcf5
Craig Eaton <craigea92@gmail.com>
2024-01-11 08:32:14 +0000

    lesson 14 complete

---
+++ 02_variables/01_variables.py
@@ -0,0 +1,25 @@
+"""
+VARIABLES
+
+Variables are how we store data in our program. 
+So far we've been directly printing data by passing it directly into the print() function.
+
+Now we are going to learn to save the data in variables so we can use and change it before we need to print it.
+
+A variable is a name that we define that will point to some data. 
+For example, I could define a new variable called my_height and set its value to 100. 
+I could also define a variable called my_name and set it equal to "Lane".
+"""
+
+# CREATING VARIABLES
+# To create a new variable in Python we use the following syntax:
+
+my_new_variable_two = 2
+this_can_be_called_anything = 3
+
+# Assignment
+# Create a new variable named player_health and set it to 1000
+
+# Solution
+player_health = 1000
+print(player_health)

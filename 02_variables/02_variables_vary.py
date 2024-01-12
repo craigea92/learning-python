504f9be7d10a1a11bbdd1195fbdc88db4c2e8e91
Craig Eaton <craigea92@gmail.com>
2024-01-12 08:32:14 +0000

    lesson 15 complete

---
+++ 02_variables/02_variables_vary.py
@@ -0,0 +1,43 @@
+"""
+VARIABLES VARY
+
+Variables are called "variables" because they can hold any value and that value can change (it varies).
+
+For example, the following will print 20:
+
+acceleration = 10
+acceleration = 20
+print(acceleration)
+
+The line acceleration = 20 reassigns the value of acceleration to 20. 
+It overwrites whatever was being held in the acceleration variable before.
+"""
+
+# Assignment
+# Before each print() function in the provided code, change the value of player_health to 100 less than it was before.
+
+player_health = 1000
+print(player_health)
+
+# here
+print(player_health)
+
+# here
+print(player_health)
+
+# here
+print(player_health)
+
+# Solution
+player_health = 1000
+print(player_health)
+
+player_health = 900
+print(player_health)
+
+player_health = 800
+print(player_health)
+
+player_health = 700
+print(player_health)
+

1eab290825d5086736b5225baa9cd76442401406
Craig Eaton <craigea92@gmail.com>
2024-01-13 08:32:14 +0000

    lesson 16 complete

---
+++ 02_variables/03_some_math.py
@@ -0,0 +1,22 @@
+"""
+LET'S DO SOME MATH
+
+Now that we know how to store and change the value of variables let's do some maths!
+
+Here are some examples of common mathematical operators in Python syntax
+
+sum = a + b
+difference = a - b
+product = a * b
+quotient = a / b
+"""
+
+# Assignment
+# Create a variable called armored_health and set it equal to player_health * armor_multiplier
+
+# Solution
+player_health = 1000
+armor_multiplier = 2
+
+armored_health = player_health * armor_multiplier
+print(armored_health)

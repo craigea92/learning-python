a534a90416a5fd4751e1271df97a1753b2a20c52
Craig Eaton <craigea92@gmail.com>
2024-01-14 08:32:14 +0000

    lesson 17 complete

---
+++ 02_variables/04_more_math.py
@@ -0,0 +1,24 @@
+"""
+LET'S DO SOME MORE MATH
+
+Creating negative numbers in Python works the way you probably expect. 
+Just add a minus sign in front of a number.
+
+my_negative_num = -1
+"""
+
+# Assignment
+# Change the poison_damage variable to be negative
+
+player_health = 100
+poison_damage = 10
+
+player_poison_health = player_health + poison_damage
+print(player_poison_health)
+
+# Solution
+player_health = 100
+poison_damage = -10
+
+player_poison_health = player_health + poison_damage
+print(player_poison_health)

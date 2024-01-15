aa14af6a846f3b37d65539b820acf4bec9467c1d
Craig Eaton <craigea92@gmail.com>
2024-01-15 08:32:14 +0000

    lesson 18 complete

---
+++ 02_variables/05_comments.py
@@ -0,0 +1,27 @@
+"""
+COMMENTS
+
+Comments don't run like code, they are ignored by the computer.
+Comments are useful for adding reminders or explaining what a piece of code does in plain English.
+
+SINGLE LINE COMMENT
+
+# A hash is used for single line comments
+
+MULTI-LINE COMMENTS (DOCSTRINGS)
+
+""" """ Triple quotes are used to start and end multi-line comments.
+This is useful if you don't want to add the # to the start of each line when writing paragraphs.
+"""
+
+# Assignment
+# Fix the bug by commenting out the first line
+
+the best_sword variable holds the value of the best sword in the game
+best_sword = "scimitar"
+print(best_sword)
+
+# Solution
+# the best_sword variable holds the value of the best sword in the game
+best_sword = "scimitar"
+print(best_sword)

820ac95119406c3973862ebee1200cdc30a23ec2
Craig Eaton <craigea92@gmail.com>
2024-01-17 08:32:14 +0000

    lesson 20 and 21 complete

---
+++ 02_variables/07_08_variable_names.py
@@ -0,0 +1,36 @@
+"""
+VARIABLE NAMES
+
+Variable names can't have spaces, they're continuous strings of characters.
+
+In Python you should use "snake_case" when creating variable names - it's become the rule of thumb for the language.
+By way of comparison, "camelCase" is where the beginning of each new word except the first is capitalized.
+
+CAMEL CASE
+someVariableHere = 10
+
+SNAKE CASE
+some_variable_here = 10
+"""
+
+# Question 01
+
+# Which is valid camel case?
+
+# hero_health
+# heroHealth
+# HeroHealth
+# HEROHEALTH
+
+# Answer = heroHealth
+
+# Question 02
+
+# Which is valid snake case?
+
+# hero_health
+# heroHealth
+# HeroHealth
+# HEROHEALTH
+
+# Answer = hero_health

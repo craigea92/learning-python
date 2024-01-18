06631a16e71cecda58e70c4218e5382f9f556311
Craig Eaton <craigea92@gmail.com>
2024-01-18 09:32:14 +0000

    lesson 23 complete

---
+++ 02_variables/10_nonetype_variables.py
@@ -0,0 +1,23 @@
+"""
+NONETYPE VARIABLES
+
+Not all variables have a value. We can declare an "empty" variable by setting it to None.
+
+empty = None
+
+The value of empty in this instance is None until we use the assignment operator, =, to give it a value.
+
+NONE IS NOT A SPECIFIC STRING
+
+Note that the NoneType is not the same as a string with a value of "None":
+
+my_none = None # This is a None-type
+my_none = "None" # This is a string
+"""
+
+# Assignment
+# Declare a variable named enemy and set it to None
+
+# Solution
+enemy = None
+print(enemy is None)

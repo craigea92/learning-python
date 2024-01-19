913f6e361016e028706073131821b49a573d18e3
Craig Eaton <craigea92@gmail.com>
2024-01-19 08:32:14 +0000

    lesson 24 complete

---
+++ 02_variables/11_nonetype.py
@@ -0,0 +1,19 @@
+"""
+NONETYPE
+
+As we mentioned in the last exercise, the None keyword is used to define an empty (or "null") variable or object.
+In Python, the None keyword is an object, and it is a data type of the class NoneType
+
+Common use case: representing a variable without a chosen value yet, such as in user onboarding for selecting age.
+Avoiding common mistake: When printing the "None" type, it can be confused with the string "None".
+Solution: Use the 'type()' function to determine the type of a variable, especially when debugging code.
+"""
+
+# Question 01
+
+# Why might you use a 'None" value
+
+# To create bugs
+# As the default value that will be replaced later
+
+# Answer = As the default value that will be replaced later

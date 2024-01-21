593db1b1cc3196ea2e91a341a648f76092668c5a
Craig Eaton <craigea92@gmail.com>
2024-01-21 11:32:14 +0000

    lesson 32 complete

---
+++ 02_variables/19_tims_poem.py
@@ -0,0 +1,22 @@
+"""
+TIM'S POEM
+
+Tim is working on a program that writes a poem to the console. 
+However, Tim is having trouble getting the poem to print on only two lines.
+"""
+
+# Assignment
+# Use string concatenation to print tim's poem in 2 lines
+
+part_one = "Roses are red, "
+part_two = "Python is cool, "
+part_three = "violets are blue."
+part_four = "and so are you!"
+
+# Solution
+line_one = part_one + part_three
+line_two = part_two + part_four
+
+print(line_one)
+print(line_two)
+

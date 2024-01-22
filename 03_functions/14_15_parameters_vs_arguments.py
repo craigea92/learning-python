9ad635da872448e0a96cd5b5168ef3c7c3000c28
Craig Eaton <craigea92@gmail.com>
2024-01-22 16:36:04 +0000

    lesson 46 and 47 complete

---
+++ 03_functions/14_15_parameters_vs_arguments.py
@@ -0,0 +1,33 @@
+"""
+PARAMETERS VS ARGUMENTS
+
+Parameters are the names used for inputs when defining a function. Arguments are the names of the inputs supplied when a function is called.
+
+To reiterate, arguments are the actual values that go into the function, say 42, "the dark knight" or True.
+Parameters are the names we use in the function definition to refer to those values, which at the time of writing the function, could be anything.
+
+# a and b are parameters
+def add(a, b):
+    return a + b
+
+# 5 and 6 are arguments
+sum = add(5, 6)
+"""
+
+# Question 01
+
+# _____ are the values that are passed into the function by the caller
+
+# Parameters
+# Arguments
+
+# Answer = Arguments
+
+# Question 02
+
+# _____ are the inputs specified bu the function definition
+
+# Parameters
+# Arguments
+
+# Answer = Parameters

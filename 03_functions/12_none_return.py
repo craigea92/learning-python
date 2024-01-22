317059e864437f559def6cab312ec03c95e07e1a
Craig Eaton <craigea92@gmail.com>
2024-01-22 13:34:45 +0000

    lesson 44 complete

---
+++ 03_functions/12_none_return.py
@@ -0,0 +1,27 @@
+"""
+NONE RETURN
+
+When no return value is specified in a function, it will automatically return None. 
+The following code snippets all return the same value, None:
+
+def my_func():
+    print("I do nothing")
+    return None
+
+def my_func():
+    print("I do nothing")
+    return
+
+def my_func():
+    print("I do nothing")
+"""
+
+# Question 01
+
+# What happens if there is no 'return' line in a function?
+
+# The program crashes
+# It returns no values, an attempt to capture a return value will result in errors
+# It returns None
+
+# Answer = It returns None

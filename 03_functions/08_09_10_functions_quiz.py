0982c7cbb2aa74bf4a54107e530db7d38a293c72
Craig Eaton <craigea92@gmail.com>
2024-01-22 11:56:07 +0000

    lesson 40, 41 and 42 complete

---
+++ 03_functions/08_09_10_functions_quiz.py
@@ -0,0 +1,55 @@
+"""
+FUNCTIONS QUIZ
+
+Reference the example functions and answer the questions
+
+Function Definitions
+
+def calculate_damage(enemy_one_dmg, enemy_two_dmg, enemy_three_dmg):
+    total = enemy_one_dmg + enemy_two_dmg + enemy_three_dmg
+    return total
+
+def to_celsius(f):
+    c = 5 / 9 * (f - 32)
+    return c
+
+Function Calls
+
+damage1 = calculate_damage(10, 20, 30)
+damage2 = calculate_damage(5, 10, 15)
+
+temp1 = to_celsius(32)
+temp2 = to_celsius(212)
+"""
+
+# Question 01
+
+# Which keyword is used to create functions in Python?
+
+# function
+# func
+# fn
+# def
+
+# Answer = def
+
+# Question 02
+
+# Which is NOT an alternative term for values that are passed into a function?
+
+# slots
+# parameters
+# arguments
+
+# Answer = slots
+
+# Question 03
+
+# Once a function haas been defined _____ it can be called _____
+
+# once, once
+# once, many times
+# many times, once
+# many times, many times
+
+# Answer = once, many times

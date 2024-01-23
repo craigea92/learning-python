71cb70158ae1db2f0b78eb6ff87a0e42e05e2756
Craig Eaton <craigea92@gmail.com>
2024-01-23 19:22:22 +0000

    lesson 57 complete

---
+++ 05_testing_and_debugging/02_two_lesson_types.py
@@ -0,0 +1,17 @@
+"""
+TWO LESSON TYPES
+
+There are 2 types of coding lessons:
+
+Console output lessons
+Unit-test lessons
+"""
+
+# Question 01
+
+# You can leave in debugging code when you submit ____ lessons.
+
+# Unit test
+# Console output
+
+# Answer = Unit test

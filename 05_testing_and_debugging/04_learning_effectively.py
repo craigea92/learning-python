585cb395dc8286de80aa6c659bdc5ccc0f493549
Craig Eaton <craigea92@gmail.com>
2024-01-24 10:05:41 +0000

    lesson 59 complete

---
+++ 05_testing_and_debugging/04_learning_effectively.py
@@ -0,0 +1,25 @@
+"""
+LEARNING EFFECTIVELY
+
+There's no way around it, if programming were a walk in the park everyone would be earning 6 figures as a software engineer. 
+But it's not, and to succeed without getting stuck and frustrated you need to learn how to learn.
+
+RED, GREEN, REFACTOR
+
+The red, green, refactor approach helps developers compartmentalize their focus into three phases:
+
+Red — think about what you want to develop
+Green — think about how to make your tests pass
+Refactor — think about how to improve your existing implementation
+
+Don't be afraid to add print() statements while writing code, it may help later when debugging!
+"""
+
+# Question 01
+
+# Why do software developers refactor their code?
+
+# Improve readability and reduce complexities
+# For fun!
+
+# Answer = Improve readability and reduce complexities

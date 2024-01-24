683ebfdeced4d5008673c565019bbf40842f1856
Craig Eaton <craigea92@gmail.com>
2024-01-24 13:33:36 +0000

    lesson 66 complete

---
+++ 06_computing/03_floor_division.py
@@ -0,0 +1,23 @@
+"""
+FLOOR DIVISION
+
+Python has great out-of-the-box support for mathematical operations. 
+This, among other reasons, is why it has had such success in artificial intelligence, machine learning, and data science applications.
+
+Floor division is like normal division except the result is floored afterward, which means the remainder is removed. 
+As you would expect, this means the result is an integer instead of a float.
+
+7 // 3
+# 2 (an integer)
+"""
+
+# Question 01
+
+# What would be the result of 11 // 2 ?
+
+# 6
+# 5.5
+# 5.0
+# 5
+
+# Answer = 5

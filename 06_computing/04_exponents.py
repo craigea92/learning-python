5d896bb0bee59ac9acf7e8ef2273f151735462c4
Craig Eaton <craigea92@gmail.com>
2024-01-24 13:37:39 +0000

    lesson 67 complete

---
+++ 06_computing/04_exponents.py
@@ -0,0 +1,21 @@
+"""
+EXPONENTS
+
+Python has built-in support for exponents - something most languages require a math library for.
+
+# reads as "three squared" or
+# "three raised to the second power"
+3 ** 2
+# 9
+"""
+
+# Question 01
+
+# What is 2 ** 3?
+
+# 8
+# 6
+# 5
+# 4
+
+# Answer = 8

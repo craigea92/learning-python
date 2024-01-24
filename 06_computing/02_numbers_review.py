b9257ffdb9b3246ffa176cadb8e87876c5025dc1
Craig Eaton <craigea92@gmail.com>
2024-01-24 13:21:30 +0000

    lesson 65 complete

---
+++ 06_computing/02_numbers_review.py
@@ -0,0 +1,25 @@
+"""
+NUMBERS REVIEW
+
+INTEGERS
+In Python, numbers without a decimal part are called Integers.
+
+Integers are simply whole numbers, positive or negative. For example, 3 and -3 are both examples of integers.
+
+FLOATS
+A float is, as you may have guessed, the number type that allows for decimal values.
+
+my_int = 5
+my_float = 5.5
+"""
+
+# Question 01
+
+# _____ and _____ are numerical types in Python.
+
+# Integers, Numbers
+# Integers, FLoats
+# Integers, Doubles
+# Floats, Doubles
+
+# Answer = Integers, Floats

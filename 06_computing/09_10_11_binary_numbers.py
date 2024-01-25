255b91635fe09840e53ac16b54c84e7c3053ab9c
Craig Eaton <craigea92@gmail.com>
2024-01-25 16:37:07 +0000

    lesson 72, 73 and 74 complete

---
+++ 06_computing/09_10_11_binary_numbers.py
@@ -0,0 +1,51 @@
+"""
+BINARY NUMBERS
+
+Binary numbers are just "base 2" numbers. They work the same way as "normal" base 10 numbers, but with 2 symbols instead of 10.
+
+Each 1 in a binary number represents a greater multiple of 2. 
+In a 4-digit number, that means you have the eight's place, the four's place, the two's place, and the one's place. 
+Similar to how in decimal you would have the thousandth's place, the hundredth's place, the ten's place, and the one's place.
+
+0001 = 1
+0010 = 2
+0011 = 3
+0100 = 4
+0101 = 5
+0110 = 6
+0111 = 7
+1000 = 8
+"""
+
+# Question 01
+
+# What does the binary number 1100 represent in decimal?
+
+# 8
+# 15
+# 13
+# 12
+
+# Answer = 12
+
+# Question 02
+
+# 1101 is equivalent to which number in decimal?
+
+# 8
+# 15
+# 13
+# 12
+
+# Answer = 13
+
+# Question 03
+
+# What's the largest number you can store in 5 digits of binary?
+
+# 64
+# 63
+# 32
+# 31
+
+# Answer = 31

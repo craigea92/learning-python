9e91800bc64e0008b6fde957aa52d818d8af78bb
Craig Eaton <craigea92@gmail.com>
2024-01-26 21:58:25 +0000

    lesson 89 complete

---
+++ 07_comparisons/10_boolean_quiz.py
@@ -0,0 +1,19 @@
+"""
+BOOLEAN QUIZ
+
+Refer to the following boolean statement.
+"""
+is_tall = True
+is_fat = True
+is_skinny = False
+is_short = False
+result = (is_tall and is_fat) or (is_skinny and is_short)
+
+# Question 01
+
+# What is the value of 'result'?
+
+# True
+# False
+
+# Answer = True

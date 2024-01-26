fce7dca3e855f093b11fa1da4a4cd1acd7597ec9
Craig Eaton <craigea92@gmail.com>
2024-01-26 11:16:25 +0000

    lesson 77 complete

---
+++ 06_computing/14_not.py
@@ -0,0 +1,20 @@
+"""
+NOT
+
+The not operator reverses the result. It returns False if the input was True and vice-versa.
+
+print(not True)
+# Prints: False
+
+print(not False)
+# Prints: True
+"""
+
+# Question 01
+
+# The operators 'and', 'or', and 'not' are _____ operators
+
+# Logical
+# Bitwise
+
+# Answer = Logical

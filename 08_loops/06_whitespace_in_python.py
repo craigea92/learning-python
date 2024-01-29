f11ce31d73d50e9c185b24e9ac3fb11c942fc5f9
Craig Eaton <craigea92@gmail.com>
2024-01-29 20:09:49 +0000

    lesson 99 complete

---
+++ 08_loops/06_whitespace_in_python.py
@@ -0,0 +1,18 @@
+"""
+WHITESPACE IN PYTHON
+
+The body of a for-loop must be indented, otherwise you'll get a syntax error. 
+Not only that, but every line in the body of the loop must be indented in the same way - we use the "4 spaces" convention. 
+Pressing the <tab> key should automatically insert 4 spaces.
+
+Whitespace matters in Python.
+"""
+
+# Question 01
+
+# It is _____ to indent the body of a traditional for-loop in Python
+
+# Required
+# Optional
+
+# Answer = Required

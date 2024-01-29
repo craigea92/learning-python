f446445944f2a0af3675f293f61870b64f3b2fba
Craig Eaton <craigea92@gmail.com>
2024-01-29 20:06:33 +0000

    lesson 97 and 98 complete

---
+++ 08_loops/04_05_loop_review.py
@@ -0,0 +1,27 @@
+"""
+LOOP REVIEW
+
+for i in range(0, 10):
+    print(i)
+"""
+
+# Question 01
+
+# Why would you want to use a loop?
+
+# To do the same thing over and over without writing the same code
+# Because the computer understands loops better than repetitive code
+# Because loops speed up your app
+
+# Answer = To do the same thing over and over without writing the same code
+
+# Question 02
+
+# Why does the code on the left print 0-999 and not 0-1000?
+
+# Because print(i) only happens while i is less than or equal to 1000
+# Because print(i) only happens while i is less then (not equal to) 1000
+# Because print(i) only happens while i is greater than or equal to 1000
+# Because print(i) only happens while i is greater than (not equal to) 1000
+
+# Answer = Because print(i) only happens while i is less then (not equal to) 1000

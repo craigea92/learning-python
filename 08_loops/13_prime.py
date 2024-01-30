03330e08fd293ae6f08e83d44c8701b96a773645
Craig Eaton <craigea92@gmail.com>
2024-01-30 22:09:09 +0000

    lesson 106 complete

---
+++ 08_loops/13_prime.py
@@ -0,0 +1,73 @@
+"""
+PRIME?
+
+A math student is working on a project and needs an easy way to determine whether or not a number is prime. 
+She asked you to write a program to help her.
+"""
+
+# Assignment
+# Write a function that takes a single number as input and returns True if it is a prime number or False if it is not.
+
+# Solution
+def is_prime(number):
+    if number <= 1:
+        return False
+    elif number == 2:
+        return True
+    elif number % 2 == 0:
+        return False
+    elif number % 3 == 0:
+        return False
+    return True
+
+# Tests
+run_cases = [
+    (7, True),
+    (-7, False),
+    (9, False),
+    (23, True),
+]
+
+submit_cases = run_cases + [
+    (-1, False),
+    (0, False),
+    (1, False),
+    (2, True),
+    (4, False),
+    (31, True),
+    (100, False),
+]
+
+def test(input1, expected_output):
+    print("---------------------------------")
+    print(f"Input number: {input1}")
+    print(f"Expecting: {expected_output}")
+    result = is_prime(input1)
+    print(f"Actual: {result}")
+    if result == expected_output:
+        print("Pass")
+        return True
+    print("Fail")
+    return False
+
+def main():
+    passed = 0
+    failed = 0
+    for test_case in test_cases:
+        correct = test(*test_case)
+        if correct:
+            passed += 1
+        else:
+            failed += 1
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    print(f"{passed} passed, {failed} failed")
+
+
+test_cases = submit_cases
+if "__RUN__" in globals():
+    test_cases = run_cases
+
+main()

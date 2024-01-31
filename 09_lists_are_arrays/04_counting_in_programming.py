3b8cb7772686c37ed6f088bf3b3015642052a759
Craig Eaton <craigea92@gmail.com>
2024-01-31 11:39:40 +0000

    lesson 109 complete

---
+++ 09_lists_are_arrays/04_counting_in_programming.py
@@ -0,0 +1,31 @@
+"""
+COUNTING IN PROGRAMMING
+
+In the world of programming, counting is a bit strange!
+
+We don't start counting at 1, we start at 0 instead.
+
+INDEXES
+
+Each item in a list has an index that refers to its spot in the list.
+
+Take the following list as an example:
+
+names = ["Bob", "Lane", "Alice", "Breanna"]
+
+Index 0: Bob
+Index 1: Lane
+Index 2: Alice
+Index 3: Breanna
+"""
+
+# Question 01
+
+# Which index refers to the second item in a list?
+
+# 4
+# 2
+# 1
+# -1
+
+# Answer = 1

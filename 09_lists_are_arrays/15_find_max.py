82a29d76919a9af24569a4f3513cb9b40b4b4e05
Craig Eaton <craigea92@gmail.com>
2024-02-02 11:51:56 +0000

    lesson 120 complete

---
+++ 09_lists_are_arrays/15_find_max.py
@@ -0,0 +1,66 @@
+"""
+FIND MAX
+
+INFINITY
+The built-in float() function can be used to create a numeric floating point value that represents the negative infinity value. 
+Because every value will be greater than negative infinity we can use it to help us accomplish our goal of finding the max value. 
+
+negative_infinity = float('-inf')
+positive_infinity = float('inf')
+"""
+
+# Assignment
+# Complete the find_max function that looks at each number in the nums list and returns the maximum value. 
+# If no maximum is found it just returns negative infinity.
+
+# Solution
+def find_max(nums):
+    max_so_far = float("-inf")
+    for num in nums:
+        if num > max_so_far:
+            max_so_far = num
+    return max_so_far
+
+# Tests
+run_cases = [([1, 2, 3, 4, 5], 5), ([1, 2, 300, 4, 5], 300)]
+
+submit_cases = run_cases + [
+    ([1, 20, 3, 4, 5], 20),
+    ([-1, 2, 3, 4, 5], 5),
+    ([1, 2, 3, 21, 18], 21),
+    ([], float("-inf")),
+    ([-1, -2, -3, -4, -5], -1),
+]
+
+def test(input1, expected_output):
+    print("---------------------------------")
+    print(f"Inputs: {input1}")
+    print(f"Expecting: {expected_output}")
+    result = find_max(input1)
+    print(f"Actual: {result}")
+    if result == expected_output:
+        print("Pass")
+        return True
+    print("Fail")
+    return False
+
+def main():
+    passed = 0
+    failed = 0
+    for test_case in test_cases:
+        correct = test(*test_case)
+        if correct:
+            passed += 1
+        else:
+            failed += 1
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    print(f"{passed} passed, {failed} failed")
+
+test_cases = submit_cases
+if "__RUN__" in globals():
+    test_cases = run_cases
+
+main()

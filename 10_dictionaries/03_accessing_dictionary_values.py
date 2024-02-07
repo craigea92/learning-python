0a7d362937c38e84234ddf53f297faa9b378abbc
Craig Eaton <craigea92@gmail.com>
2024-02-07 14:33:43 +0000

    lesson 137 complete

---
+++ 10_dictionaries/03_accessing_dictionary_values.py
@@ -0,0 +1,24 @@
+"""
+ACCESSING DICTIONARY VALUES
+
+Dictionary elements must be accessible somehow in code, otherwise they wouldn't be very useful.
+A value is retrieved from a dictionary by specifying its corresponding key in square brackets. 
+The syntax looks similar to indexing into a list.
+
+car = {
+    'make': 'tesla',
+    'model': '3'
+}
+print(car['make'])
+# Prints: tesla
+"""
+
+# Question 01
+
+# Which kind of brackets are used to index into a dictionary?
+
+# Square
+# Parentheses
+# Curly
+
+# Answer = Square

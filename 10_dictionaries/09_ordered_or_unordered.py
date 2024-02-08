b212cef28065625b521f1ea991cc4a5138712157
Craig Eaton <craigea92@gmail.com>
2024-02-08 14:29:55 +0000

    lesson 143 complete

---
+++ 10_dictionaries/09_ordered_or_unordered.py
@@ -0,0 +1,20 @@
+"""
+ORDERED OR UNORDERED?
+
+As of Python version 3.7, dictionaries are ordered. In Python 3.6 and earlier, dictionaries were unordered.
+
+Because dictionaries are ordered, the items have a defined order, and that order will not change.
+
+Unordered means that the items used to not have a defined order, so you couldn't refer to an item by using an index.
+
+**The takeaway is that if you're on Python 3.7 or later, you'll be able to iterate over dictionaries in the same order every time.
+"""
+
+# Question 01
+
+# Python dictionaries before 3.7 were _
+
+# Unordered
+# Ordered
+
+# Answer = Unordered

03e080d5cf2e39f2fe228baf25440dd29837e08d
Craig Eaton <craigea92@gmail.com>
2024-02-29 11:32:40 +0000

    lesson 173 complete

---
+++ 13_challenges/08_join_strings.py
@@ -0,0 +1,67 @@
+"""
+JOIN STRINGS
+"""
+
+# Assignment
+# Write a function called join_strings() that concatenates all of its string inputs end-to-end, in order, and adds a comma between them. 
+# Don't use the .join() string method.
+
+# 'hello,my,friend'
+
+# Solution
+def join_strings(strings):
+    result = ""
+    for i in range(len(strings)):
+        result += strings[i]
+        if i != len(strings) - 1:
+            result += ","
+    return result
+
+# Tests
+run_cases = [
+    (["hello", "world"], "hello,world"),
+    (["this", "list", "is", "so", "important"], "this,list,is,so,important"),
+]
+
+submit_cases = run_cases + [
+    ([], ""),
+    (["ford", "ferrari", "tesla"], "ford,ferrari,tesla"),
+    (["musk", "satya", "cook", "bezos"], "musk,satya,cook,bezos"),
+    (["dota", "sc2", "overwatch", "diablo", "mtg"], "dota,sc2,overwatch,diablo,mtg"),
+]
+
+
+def test(input1, expected_output):
+    print("---------------------------------")
+    print(f"Input: {input1}")
+    print(f"Expecting: {expected_output}")
+    result = join_strings(input1)
+    print(f"Actual: {result}")
+    if result == expected_output:
+        print("Pass")
+        return True
+    print("Fail")
+    return False
+
+
+def main():
+    passed = 0
+    failed = 0
+    for test_case in test_cases:
+        correct = test(*test_case)
+        if correct:
+            passed += 1
+        else:
+            failed += 1
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    print(f"{passed} passed, {failed} failed")
+
+
+test_cases = submit_cases
+if "__RUN__" in globals():
+    test_cases = run_cases
+
+main()

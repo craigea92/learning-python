19505d87fcc7495dcaac0b75c71a37eabfbeead6
Craig Eaton <craigea92@gmail.com>
2024-03-02 11:32:40 +0000

    lesson 176 and 177 complete

---
+++ 14_quiz/03_04_the_zen_of_python.py
@@ -0,0 +1,46 @@
+"""
+THE ZEN OF PYTHON
+
+Tim Peters, a long time Pythonista describes the guiding principles of Python in his famous short piece, The Zen of Python.
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than right now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+"""
+
+# Question 01
+
+# An implementation that's easy to explain is a good idea
+
+# Never
+# Maybe
+# Always
+
+# Answer = Always
+
+# Question 02
+
+# There should be _ obvious way(s) to accomplish a task
+
+# two
+# one
+# none
+# many
+
+# Answer = one

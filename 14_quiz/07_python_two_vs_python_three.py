3c274b633d14ed9c99ab54d8be1a886886815b0a
Craig Eaton <craigea92@gmail.com>
2024-03-04 11:32:40 +0000

    lesson 180 complete

---
+++ 14_quiz/07_python_two_vs_python_three.py
@@ -0,0 +1,25 @@
+"""
+PYTHON 2 VS PYTHON 3
+One thing that's important to keep in mind as you continue your Python journey is that the Python ecosystem suffers from split personality syndrome. 
+Python 3 was released on December 3rd, 2008, but over a decade later the web is still full of Python 2 dependencies, scripts and tutorials.
+
+In this course, we used Python 3 - just like any good citizen should these days.
+
+One of the most obvious breaking changes between Python 2 and 3 is the syntax for printing text to the console.
+
+PYTHON 2
+print "hello world"
+
+PYTHON 3
+print("hello world")
+"""
+
+# Assignment
+# Update the code from Python 2 to Python 3
+# print "python 2 really needs to disappear already"
+
+# Solution
+print("python 2 really needs to disappear already")
+
+# Tests
+# N/A

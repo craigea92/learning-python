8130a2a51fdf5f65c40a3de118af25fd54f6851a
Craig Eaton <craigea92@gmail.com>
2024-03-05 11:32:40 +0000

    lesson 181 complete

---
+++ 14_quiz/08_should_you_use_python_three.py
@@ -0,0 +1,16 @@
+"""
+SHOULD YOU USE PYTHON 2 OR 3?
+
+As you've probably guessed, you should always use Python 3 going forward!
+
+Python 2 and 3 are similar, but Python 3 contains significant changes that are not backward compatible with the 2.x versions.
+"""
+
+# Question 01
+
+# Python 3 code is always backwards compatible with Python 2
+
+# True
+# False
+
+# Answer = False
